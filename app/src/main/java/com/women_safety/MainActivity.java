f3d92252e8213500bc794aac95ba4351b0140736
Dhruvilvyas22 <dhruvil22052000@gmail.com>
2024-03-15 12:40:40 -0400

    Build Update. Works perfectly

--- app/src/main/java/com/women_safety/MainActivity.java
+++ app/src/main/java/com/women_safety/MainActivity.java
@@ -1,32 +1,33 @@
 package com.women_safety;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button Yellow = (Button) findViewById(R.id.button2);
-        Yellow.setBackgroundColor(Color.YELLOW);
-        Button Red = (Button) findViewById(R.id.button3);
-        Red.setBackgroundColor(Color.RED);
+
+        Button yellowButton = findViewById(R.id.button2);
+        yellowButton.setBackgroundColor(Color.YELLOW);
+
+        Button redButton = findViewById(R.id.button3);
+        redButton.setBackgroundColor(Color.RED);
     }
 
-    public void Yellow_Button_Pressed(View view) {
+    public void yellowButtonPressed(View view) {
         Intent intent = new Intent(this, Yellow.class);
         startActivity(intent);
     }
 
-    public void Red_Button_Pressed(View view) {
+    public void redButtonPressed(View view) {
         Intent intent = new Intent(this, Red.class);
         startActivity(intent);
     }
